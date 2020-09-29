be5dc2f5ab3382169d05f971c7258f914bacb5a3
Reber.Han <reber@ReberHandeMacBook-Pro.local>
2020-09-29 18:32:35 +0800

    swiperefreshlayout的拷贝

--- Library-UI/src/main/java/com/base/library/ui/refresh/AppPullRefreshLayout.java
+++
@@ -1,8 +0,0 @@
-package com.base.library.ui.refresh;
-
-/**
- * @author reber
- * on 2020/9/29 17:07
- */
-public class AppPullRefreshLayout {
-}
--- app/src/main/java/com/library/test/ui/refresh/AppRefreshActivity.java
+++ app/src/main/java/com/library/test/ui/refresh/AppRefreshActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.base.library.common.activity.BaseAppActivity;
 import com.base.library.common.helper.AppViewHelper;
@@ -21,6 +22,17 @@ public class AppRefreshActivity extends BaseAppActivity {
 
     @Override
     protected void onInitCreateWithState(@NonNull AppViewHelper viewHelper, @Nullable Bundle savedInstanceState) {
-
+        final SwipeRefreshLayout refreshLayout = viewHelper.findViewById(R.id.swipe_refresh);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.setRefreshing(false);
+                    }
+                }, 2000);
+            }
+        });
     }
 }
