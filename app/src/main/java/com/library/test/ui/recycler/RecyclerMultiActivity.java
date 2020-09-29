a976b55db42c5391448474c9e57bcb97e0a4c53b
Reber.Han <reber@ReberHandeMacBook-Pro.local>
2020-09-29 17:56:29 +0800

    recyclerView的多类型adapter的测试

--- app/src/main/java/com/library/test/ui/recycler/RecyclerMultiActivity.java
+++ app/src/main/java/com/library/test/ui/recycler/RecyclerMultiActivity.java
@@ -1,13 +1,22 @@
 package com.library.test.ui.recycler;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.base.library.common.activity.BaseAppActivity;
 import com.base.library.common.helper.AppViewHelper;
+import com.base.library.ui.recycler.AppRecyclerView;
+import com.base.library.ui.recycler.adapter.AppMultiTypeAdapter;
+import com.base.library.ui.recycler.decoration.AppLinearItemDecoration;
 import com.library.test.R;
+import com.library.test.ui.recycler.item.RecyclerMultiItemFirst;
+import com.library.test.ui.recycler.item.RecyclerMultiItemFourth;
+import com.library.test.ui.recycler.item.RecyclerMultiItemSecond;
+import com.library.test.ui.recycler.item.RecyclerMultiItemThird;
 
 /**
  * @author reber
@@ -21,6 +30,22 @@ public class RecyclerMultiActivity extends BaseAppActivity {
 
     @Override
     protected void onInitCreateWithState(@NonNull AppViewHelper viewHelper, @Nullable Bundle savedInstanceState) {
+        AppRecyclerView recyclerView = viewHelper.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new AppLinearItemDecoration.Builder()
+                .setRVBoundaryValue(new Rect(10, 50, 10, 20))
+                .setItemDivideValue(R.dimen.dp_2)
+                .build(this));
 
+        AppMultiTypeAdapter adapter = new AppMultiTypeAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.insertItem(new RecyclerMultiItemFirst());
+        adapter.insertItem(new RecyclerMultiItemSecond());
+        adapter.insertItem(new RecyclerMultiItemThird());
+        adapter.insertItem(new RecyclerMultiItemThird());
+        adapter.insertItem(new RecyclerMultiItemThird());
+        adapter.insertItem(new RecyclerMultiItemFourth());
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/library/test/ui/recycler/item/RecyclerMultiItemSecond.java
@@ -0,0 +1,21 @@
+package com.library.test.ui.recycler.item;
+
+import com.base.library.ui.recycler.holder.AppViewHolder;
+import com.base.library.ui.recycler.item.AppAdapterItem;
+import com.library.test.R;
+
+/**
+ * @author reber
+ * on 2020/9/29 17:33
+ */
+public class RecyclerMultiItemSecond extends AppAdapterItem {
+    @Override
+    public int getLayoutResId() {
+        return R.layout.item_recycler_multi_second;
+    }
+
+    @Override
+    public <T> void onBindViewHolder(AppViewHolder<T> holder) {
+
+    }
+}
